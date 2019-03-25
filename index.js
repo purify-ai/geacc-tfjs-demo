069adfc3362ae7927487f97001225cb30935b690
Rustam Aliyev <rustam.aliyev@onedome.com>
2019-03-25 15:21:48 +0000

    Update to tfjs 1.0

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ let mobilenet;
 const mobilenetDemo = async () => {
 	status('Loading model...');
 
-	mobilenet = await tf.loadModel(MOBILENET_MODEL_PATH);
+	mobilenet = await tf.loadLayersModel(MOBILENET_MODEL_PATH);
 
 	// Warmup the model. This isn't necessary, but makes the first prediction
 	// faster. Call `dispose` to release the WebGL memory allocated for the return
